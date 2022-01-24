56108f6a4af8b591b3dd036a2d2e5320e5c002fb
DESKTOP-MDEFT09\Muhammet Korçil <korcilmuhammet@gmail.com>
2022-01-24 22:20:30 +0300

    string methods

--- StringMethodExamples/Program.cs
+++ StringMethodExamples/Program.cs
@@ -6,7 +6,26 @@ namespace StringMethodExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sentence = "Hello world! I am here";
+
+            var result = sentence.Length;
+            var result2 = sentence.ToUpper();
+            var result3 = sentence.ToLower();
+            var contains = sentence.Contains("world");
+            Console.WriteLine(contains);
+            var startsWith = sentence.StartsWith("Hel");
+            Console.WriteLine(startsWith);
+            var indexOf = sentence.IndexOf("world");
+            Console.WriteLine(indexOf);
+            var lastIndexOf = sentence.LastIndexOf("I");
+            Console.WriteLine(lastIndexOf);
+            var insert = sentence.Insert(5, " (hi)");
+            Console.WriteLine(insert);
+            var replace = sentence.Replace("!", ",");
+            Console.WriteLine(replace);
+            var substring = sentence.Substring(6, 9);
+            Console.WriteLine(substring);
+
 
         }
     }
